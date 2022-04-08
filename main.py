837263f7a5aa4946884b24980bdd74005de198b0
unknown <louisjaeckle1@gmail.com>
2022-04-08 17:30:00 -0600

    changed plotting features

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ class Connectome:
         self.coordinates = self.coordinates[reorder]
         self.names = self.names[reorder]
 
-    def plot_cube(self, bounds, cube_dim, points_size=30):
+    def plot_cube(self, bounds, cube_dim):
         # 3d plots subgraph in cube given by bounds
         # bounds = [xbound,ybound,zbound], cube dim is side size of square
         # returns whether succeeded
@@ -74,15 +74,24 @@ class Connectome:
         if len(node_names) == 0:
             return False
 
-        seen_nodes = set()
+        self.plot_nodes(node_names)
 
-        node_pairs = []  # list of connected [node,node] pairs
+    #use this to 3d plot null and empirical networks
+    def plot_nodes(self,node_names,edges=None):
+        #node_names - all nodes to be plotted
+        #edges- list of edges. if None, will be taken from empirical network
 
-        for node in node_names:
-            for neighbor in self.G.neighbors(node):
-                if neighbor in seen_nodes:
-                    node_pairs.append([node, neighbor])
-            seen_nodes.add(node)
+
+        if edges==None:
+            print('edges==none')
+            edges = []
+            seen_nodes = set()
+            for node in node_names:
+                for neighbor in self.G.neighbors(node):
+                    if neighbor in seen_nodes:
+                        edges.append([node, neighbor])
+                seen_nodes.add(node)
+
         x_plots = [self.node_dict[name][0] for name in node_names]
         y_plots = [self.node_dict[name][1] for name in node_names]
         z_plots = [self.node_dict[name][2] for name in node_names]
@@ -92,28 +101,25 @@ class Connectome:
         ax.set_ylabel('Y')
         ax.set_zlabel('Z')
 
-        ax.scatter(x_plots, y_plots, z_plots, s=points_size, color='blue')
+        ax.scatter(x_plots, y_plots, z_plots, s=30, color='blue')
 
         # uncomment to display node labels
         #         for node in node_names:
         #             ax.text(*self.node_dict[node],node,size=10,zorder=1,color='k')
 
-        for node1, node2 in node_pairs:
+        for node1, node2 in edges:
             plt_args = [[self.node_dict[node1][i], self.node_dict[node2][i]] for i in range(3)]
             ax.plot(*plt_args)
         plt.show()
 
         return True
 
-    def plot_random_cube(self,cube_dim, points_size=None):
+    def plot_random_cube(self,cube_dim):
         #3d plots subnetwork in cube with random bounds
         success = False
         while not success:
             bounds = self.get_random_bounds(cube_dim)
-            if points_size==None:
-                success = self.plot_cube(bounds,cube_dim)
-            else:
-                success = self.plot_cube(bounds,cube_dim,points_size=points_size)
+            success = self.plot_cube(bounds,cube_dim)
 
     # get random bounds for a cube to parse
     def get_random_bounds(self, cube_dim):
@@ -125,17 +131,18 @@ class Connectome:
         # bounds = [xbound,ybound,zbound], cube dim is side size of square
         # returns names, locations sorted by ascending z coordinates (ndarrays)
         lo_ind, hi_ind = 0, 0
-        locations = self.coordinates
-        names = self.names
+        locations = np.copy(self.coordinates)
+        names = np.copy(self.names)
+
         for ind in range(3):
             lo_ind = binary_search(locations, bounds[ind], ind)
             if lo_ind == -1:
                 #                 print('no nodes in range')#no nodes left
-                return [], []
+                return np.array([]), np.array([])
             hi_ind = binary_search(locations, bounds[ind] + cube_dim, ind)
             if hi_ind <= lo_ind:
                 #                 print('no nodes in range')#no nodes left
-                return [], []
+                return np.array([]), np.array([])
             locations = locations[lo_ind:hi_ind]
             names = names[lo_ind:hi_ind]
             if (ind != 2):
@@ -151,10 +158,14 @@ class Connectome:
         # formated like output of get_subgraph (ie sorted by ascending z coordinates)
         # dim is size of cube, in terms of neurons per side
 
-        # return flattened arrays degrees,adjacencies, which are representations of the degree structure and adjacency matrix
+        # return flattened arrays degrees,adjacencies,name which are representations of the degree structure and adjacency matrix and a list of names in order
+        #and returns name
+
         if len(names) < dim ** 3:
             #             print('not enough nodes to parse')
-            return np.array([]), np.array([])
+            return np.array([]), np.array([]), np.array([])
+
+        parsed_names = []
         degrees = np.zeros(dim ** 3)
         adj_matrix = [[0 for _ in range(row_len)] for row_len in
                       range(dim ** 3)]  # 2d list indexed by [higher node index],[lower node index]
@@ -178,16 +189,17 @@ class Connectome:
                             degrees[node_indices[neighbor]] += 1
                             adj_matrix[node_index][node_indices[neighbor]] = 1
                     node_indices[row_names[col]] = node_index
+                    parsed_names.append(row_names[col])
 
         adj_matrix = [val for adj_row in adj_matrix for val in adj_row]
-        return degrees, np.asarray(adj_matrix)
+        return degrees, np.asarray(adj_matrix),np.asarray(parsed_names)
 
     # gets and parses subgraph in cube with given bounds
     def get_and_parse(self, cube_dim, parse_dim, bounds):
         # return flattened ndarrays degrees,adjacencies, which are representations of the degree structure and adjacency matrix
         names, locations = self.get_cube(bounds, cube_dim)
-        degrees, adjacencies = self.parse_cube(names, locations, parse_dim)
-        return degrees, adjacencies
+        degrees, adjacencies,names = self.parse_cube(names, locations, parse_dim)
+        return degrees, adjacencies,np.asarray(names)
 
     # get flattened degrees, flattened adjacencies of a random cube
     def random_get_and_parse(self, cube_dim, parse_dim):
@@ -198,10 +210,10 @@ class Connectome:
         parsed_subgraph = None
         while True:
             bounds = self.get_random_bounds(cube_dim)
-            degrees, adjacencies = self.get_and_parse(cube_dim, parse_dim, bounds)
+            degrees, adjacencies,names = self.get_and_parse(cube_dim, parse_dim, bounds)
 
             if len(degrees) != 0:
-                return degrees, adjacencies
+                return degrees, adjacencies,names
 
     def example_generator(self,cube_dim,parse_dim,batch_size=32):
         n_nodes = int(parse_dim**3)#number of nodes per cube
@@ -209,7 +221,7 @@ class Connectome:
             train_features = np.empty((batch_size,n_nodes))
             train_labels = np.empty((batch_size,int(n_nodes*(n_nodes-1)/2)))
             for i in range(batch_size):
-                features, labels = self.random_get_and_parse(cube_dim, parse_dim)
+                features, labels,_ = self.random_get_and_parse(cube_dim, parse_dim)
                 train_features[i] = features
                 train_labels[i] = labels
             yield train_features,train_labels
